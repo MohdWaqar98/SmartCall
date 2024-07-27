4e2c36b3a34426744ccb4185649bc8b1e0ff451d
MohdWaqar98 <mohdwaqar695@gmail.com>
2024-07-27 21:30:12 +0530

    Basic Layout Builded And Initialized repo

---
+++ src/main/java/com/smartcontactmanager/Smart/Contact/Manager/controllers/PageController.java
@@ -0,0 +1,32 @@
+package com.smartcontactmanager.Smart.Contact.Manager.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home Page Handler");
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("OwnerName", "Mohd Waqar Ahmed");
+        model.addAttribute("GithubRepository", "https://github.com/MohdWaqar98");
+        return "home";
+    }
+
+    //about page
+    @RequestMapping("/about")
+    public String aboutpage(){
+        System.out.println("About Page Running");
+        return "about";
+    }  
+    
+    //services
+    @RequestMapping("/services")
+    public String servicepage(){
+        System.out.println("Service Page Running");
+        return "services";
+    }     
+
+}
